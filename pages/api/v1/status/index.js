dbc341b02a5b10ac13f41b1afd576f7751d4e392
Hallisson Mesquita <hallissonnn@gmail.com>
2026-03-15 18:31:50 +0000

    Configurando banco de dados local

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, response){
+import database from "../../../../infra/database.js"
+
+async function status(request, response){
+  const result = await database.query("SELECT 1 + 1")
+  console.log(result.rows)
   response.status(200).send("alunos do cursos.dev são pessoas acima da media")
 }
 
