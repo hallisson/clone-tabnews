88957440762605e8f1e5c1b423474f35cac4b09d
Hallisson Mesquita <hallissonnn@gmail.com>
2026-03-19 10:32:13 +0000

    finaliza o endpoint de

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,32 @@
 import database from "infra/database.js"
 
 async function status(request, response){
-  const result = await database.query("SELECT 1 + 1")
-  console.log(result.rows)
-  response.status(200).send("alunos do cursos.dev são pessoas acima da media")
+  const updatedAt = new Date().toISOString();
+  const dbVersionResult = await database.query("SHOW server_version;")
+  const dbVersionValue = dbVersionResult.rows[0].server_version
+
+  const dbMaxConnectionResult = await database.query("SHOW max_connections;")
+  const dbMaxConnectionValue = dbMaxConnectionResult.rows[0].max_connections
+
+  const dbName = process.env.POSTGRES_DB
+  const dbOpenConnectionResult = await database.query({
+    text: `SELECT count(*)::int from pg_stat_activity where datname = $1;`,
+    values: [dbName]
+  })
+
+  // const dbOpenConnectionResult = await database.query(`SELECT count(*)::int from pg_stat_activity where datname = '${dbName}';`)
+  const dbOpenConnectionValue = dbOpenConnectionResult?.rows[0]?.count
+  console.log(dbOpenConnectionResult.rows)
+  response.status(200).send({
+    updated_at: updatedAt,
+    dependencies:{
+      database: {
+        version: dbVersionValue,
+        max_connections: parseInt(dbMaxConnectionValue),
+        opened_connections: dbOpenConnectionValue,
+      }
+    }
+  })
 }
 
 export default status
