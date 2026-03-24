6ccd5eb227fa602b591a86d7308d03c7321773e5
Hallisson Mesquita <hallissonnn@gmail.com>
2026-03-24 09:26:28 +0000

    Iniciando migrations projeto

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,10 +3,10 @@ import database from "infra/database.js"
 async function status(request, response){
   const updatedAt = new Date().toISOString();
   const dbVersionResult = await database.query("SHOW server_version;")
-  const dbVersionValue = dbVersionResult.rows[0].server_version
+  const dbVersionValue = dbVersionResult?.rows[0]?.server_version
 
   const dbMaxConnectionResult = await database.query("SHOW max_connections;")
-  const dbMaxConnectionValue = dbMaxConnectionResult.rows[0].max_connections
+  const dbMaxConnectionValue = dbMaxConnectionResult?.rows[0]?.max_connections
 
   const dbName = process.env.POSTGRES_DB
   const dbOpenConnectionResult = await database.query({
@@ -16,7 +16,7 @@ async function status(request, response){
 
   // const dbOpenConnectionResult = await database.query(`SELECT count(*)::int from pg_stat_activity where datname = '${dbName}';`)
   const dbOpenConnectionValue = dbOpenConnectionResult?.rows[0]?.count
-  console.log(dbOpenConnectionResult.rows)
+  console.log(dbOpenConnectionResult?.rows)
   response.status(200).send({
     updated_at: updatedAt,
     dependencies:{
